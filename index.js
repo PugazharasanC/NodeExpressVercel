c4d0b66483c02895fb0f7ae1c083653edd28bcfa
Pugazharasan <32019083+PugazharasanC@users.noreply.github.com>
2022-12-12 12:18:00 +0530

    Init commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+const port = process.env.PORT || 8080;
+app.listen(port, function(){
+    console.log(`app is listening port ${port}`);
+})
+
+app.get("/:name",(req,res)=>{
+    const {name} = req.params;
+    res.status(200).send(`name: ${name}`)
+})
+app.get("/*",(req,res)=>{
+    res.send(`404`)
+})
